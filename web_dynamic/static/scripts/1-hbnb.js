8c45d38a12983c2d14b4bb9ea6728bf81cf37bfc
jaime <jandres.vr@hotmail.com>
2020-02-19 02:18:12 -0500

    fix: checkbox, popover and amenities h4

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
   const dict = {};
   $('input[type=checkbox]').click(function () {
+    // all amenities
     if ($(this).is(':checked')) {
       dict[$(this).data('name')] = $(this).data('id');
     } else {
       delete dict[$(this).data('name')];
     }
+    // set h4 with the amenities
     const element = $(this).closest('.amenities').find('h4');
-    const list = Object.keys(dict);
+    const list = Object.keys(dict).join(', ');
     if (list.length > 0) {
       element.text(list);
     } else {
       element.text('');
+      element.append('<br/>');
     }
   });
+  // uncheck checkbox after reload the DOM
+  $(':checkbox').prop('checked', false);
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,21 +1,23 @@
-
 $(document).ready(function () {
   const dict = {};
   $('input[type=checkbox]').click(function () {
+    // all amenities
     if ($(this).is(':checked')) {
       dict[$(this).data('name')] = $(this).data('id');
     } else {
       delete dict[$(this).data('name')];
     }
+    // set h4 with the amenities
     const element = $(this).closest('.amenities').find('h4');
-    const list = Object.keys(dict);
+    const list = Object.keys(dict).join(', ');
     if (list.length > 0) {
       element.text(list);
     } else {
       element.text('');
+      element.append('<br/>');
     }
   });
-
+  // request
   $.ajax({
     type: 'GET',
     url: 'http://0.0.0.0:5001/api/v1/status/',
@@ -27,4 +29,6 @@ $(document).ready(function () {
       $('DIV#api_status').removeClass('available');
     }
   });
+  // uncheck checkbox after reload the DOM
+  $(':checkbox').prop('checked', false);
 });
